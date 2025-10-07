3a7a8287eb456201f04c1006fc9af851353aabdc
Siboniso <sibandzesiboniso0@gmail.com>
2025-10-07 14:39:05 +0200

    Update service worker version and modify HTML title for the invoicing system

--- flutter_app/build/web/flutter_bootstrap.js
+++ flutter_app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "971847118"
+    serviceWorkerVersion: "2060191093"
   }
 });
